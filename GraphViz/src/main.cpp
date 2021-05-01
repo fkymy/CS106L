1ead12f28624af971e87d97f0ebcfb40cd67ebfd
fkymy <yuskefuku@gmail.com>
2021-05-01 21:49:10 +0900

    qt pie

--- GraphViz/src/main.cpp
+++ GraphViz/src/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "SimpleGraph.h"
 
 using std::cout;	using std::endl;
+using std::cin;     using std::string;
+using std::ifstream;
 
 void Welcome();
 
 // Main method
 int main() {
     Welcome();
+    // Prompt user for file name
+    // - should exist
+    // - if not try again
+    // Prompt user for number of seconds to run the algorithm
+    // - should be positive, int
+
+    cout << "Enter filename: ";
+    string filename;
+    std::getline(cin, filename);
+    cout << "Good job! you entered " << filename << endl;
+
+    ifstream file(filename);
+
+
     return 0;
 }
 
@@ -18,4 +36,24 @@ void Welcome() {
     cout << "This program uses a force-directed graph layout algorithm" << endl;
     cout << "to render sleek, snazzy pictures of various graphs." << endl;
     cout << endl;
+}
+
+string getFilename() {
+    while (true) {
+        cout << "Enter filename: ";
+        string filename;
+        std::getline(cin, filename, '\n');
+        if (!filename.empty()) {
+            return filename;
+        } else {
+            cout << "filename is empty" << endl;
+        }
+    }
+}
+
+int getSeconds() {
+    cout << "Enter seconds: ";
+    int seconds;
+    cin >> seconds;
+    return seconds;
 }
