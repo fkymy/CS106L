c17c3cfa149f6931e5a0b8534dcda7302e3a5ad4
fkymy <yuskefuku@gmail.com>
2021-05-04 03:16:38 +0900

    fix delta bug

--- GraphViz/src/main.cpp
+++ GraphViz/src/main.cpp
@@ -23,6 +23,7 @@ void printVector(const std::vector<T> &v);
 const double kPi = 3.14159265358979323;
 const double numSeconds = 15;
 const double kRepel = 0.001;
+const double kAttract = 0.001;
 
 int main() {
     SimpleGraph graph;
@@ -61,8 +62,8 @@ void parse(SimpleGraph &graph, string filename) {
     for (size_t i = 0; i < numNodes; ++i) {
         Node node;
         // Evenly space out on unit circle
-        node.x = cos((2 * kPi * (i + 1)) / numNodes);
-        node.y = sin((2 * kPi * (i + 1)) / numNodes);
+        node.x = cos((2 * kPi * i) / numNodes);
+        node.y = sin((2 * kPi * i) / numNodes);
         graph.nodes.push_back(node);
     }
 
@@ -85,37 +86,46 @@ void iterativelyDraw(SimpleGraph &graph, double numSeconds) {
     startTime = time(NULL);
     elapsedTime = difftime(time(NULL), startTime);
     while (elapsedTime < numSeconds) {
-        // do complex operation
         computeForce(graph);
         DrawGraph(graph);
         elapsedTime = difftime(time(NULL), startTime);
     }
+    cout << "DONE!" << endl;
 }
 
 void computeForce(SimpleGraph &graph) {
+    std::vector<double> deltaX(graph.nodes.size());
+    std::vector<double> deltaY(graph.nodes.size());
+
     for (size_t i = 0; i < graph.nodes.size() - 1; ++i) {
         for (size_t j = i + 1; j < graph.nodes.size(); ++j) {
             double xDiff = graph.nodes[j].x - graph.nodes[i].x;
             double yDiff = graph.nodes[j].y - graph.nodes[i].y;
-            double fRepel = kRepel / sqrt(xDiff * xDiff + yDiff * yDiff);
+            double fRepel = kRepel / sqrt(yDiff * yDiff + xDiff * xDiff);
             double theta = atan2(yDiff, xDiff);
-            graph.nodes[i].x -= fRepel * cos(theta);
-            graph.nodes[i].y -= fRepel * sin(theta);
-            graph.nodes[j].x += fRepel * cos(theta);
-            graph.nodes[j].y += fRepel * sin(theta);
+            deltaX[i] -= fRepel * cos(theta);
+            deltaY[i] -= fRepel * sin(theta);
+            deltaX[j] += fRepel * cos(theta);
+            deltaY[j] += fRepel * sin(theta);
         }
     }
     for (size_t i = 0; i < graph.edges.size(); ++i) {
-        Node start = graph.nodes[graph.edges[i].start];
-        Node end = graph.nodes[graph.edges[i].end];
+        size_t startIndex = graph.edges[i].start;
+        size_t endIndex = graph.edges[i].end;
+        Node start = graph.nodes[startIndex];
+        Node end = graph.nodes[endIndex];
         double xDiff = end.x - start.x;
         double yDiff = end.y - start.y;
-        double fAttract = kRepel * (xDiff * xDiff + yDiff * yDiff);
+        double fAttract = kAttract * (yDiff * yDiff + xDiff * xDiff);
         double theta = atan2(yDiff, xDiff);
-        start.x += fAttract * cos(theta);
-        start.y += fAttract * sin(theta);
-        end.x -= fAttract * cos(theta);
-        end.x -= fAttract * sin(theta);
+        deltaX[startIndex] += fAttract * cos(theta);
+        deltaY[startIndex] += fAttract * sin(theta);
+        deltaX[endIndex] -= fAttract * cos(theta);
+        deltaY[endIndex] -= fAttract * sin(theta);
+    }
+    for (size_t i = 0; i < graph.nodes.size(); ++i) {
+        graph.nodes[i].x += deltaX[i];
+        graph.nodes[i].y += deltaY[i];
     }
 }
 
