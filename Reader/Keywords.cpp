85593c9b444b569ef2378fca90b31104eb6150e7
fkymy <yuskefuku@gmail.com>
2021-05-07 14:01:11 +0900

    happy friday!

--- Reader/Keywords.cpp
+++ Reader/Keywords.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-#include <string>
 #include <fstream>
+#include <iostream>
 #include <map>
+#include <set>
+#include <sstream>
+#include <string>
 using namespace std;
 
+string GetLine();
 void OpenUserFile(ifstream& fileStream);
 
 string GetFileContents(ifstream& file);
@@ -18,7 +21,72 @@ int main() {
 
     for (map<string, size_t>::iterator itr = report.begin();
          itr != report.end(); ++itr) {
-        cout << "Keyword " << itr->first << " occurred "
-            << itr->second << " times." << endl;
+        cout << "Keyword " << itr->first << " occurred " << itr->second
+             << " times." << endl;
     }
+}
+
+string GetLine() {
+    string line;
+    getline(cin, line);
+    return line;
+}
+
+void OpenUserFile(ifstream& input) {
+    while (true) {
+        cout << "Enter filename: ";
+        string filename = GetLine();
+
+        input.open(filename.c_str());
+        if (input.is_open()) return;
+
+        cout << "Sorry, I can't find the file " << filename << endl;
+        input.clear();
+    }
+}
+
+string GetFileContents(ifstream& file) {
+    string result;
+    string line;
+
+    while (file >> line) {
+        result += line + "\n";
+    }
+
+    return result;
+}
+
+set<string> LoadKeywords() {
+    ifstream input("keywords.txt");
+    set<string> result;
+
+    string keyword;
+    while (input >> keyword) {
+        result.insert(keyword);
+    }
+
+    return result;
+}
+
+void PreprocessString(string& text) {
+    for (size_t k = 0; k < text.size(); ++k)
+        if (ispunct(text[k]) && text[k] != '_') text[k] = ' ';
+}
+
+map<string, size_t> GenerateKeywordReport(string contents) {
+    set<string> keywords = LoadKeywords();
+
+    PreprocessString(contents);
+
+    stringstream tokenizer;
+    tokenizer << contents;
+
+    map<string, size_t> result;
+
+    string word;
+    while (tokenizer >> word) {
+        if (keywords.count(word)) ++result[word];
+    }
+
+    return result;
 }
--- Reader/Snake.cpp
+++ Reader/Snake.cpp
@@ -1,213 +1,211 @@
-#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream>
 #include <string>
 #include <vector>
-#include <deque>
-#include <ctime>
-#include <cstdlib>
 
 using namespace std;
 
 const int kMaxFood = 20;
 
 const char kEmptyTile = ' ';
-const char kWallTile  = '#';
-const char kFoodTile  = '$';
+const char kWallTile = '#';
+const char kFoodTile = '$';
 const char kSnakeTile = '*';
 
 struct pointT {
-	int row, col;
+    int row, col;
 };
 
 struct gameT {
-	vector<string> world;
-	int numRows, numCols;
+    vector<string> world;
+    int numRows, numCols;
 
-	deque<pointT> snake;
-	int dx, dy;
-	int numEaten;
+    deque<pointT> snake;
+    int dx, dy;
+    int numEaten;
 };
 
 string GetLine() {
-	string result;
-	std::getline(cin, result);
-	return result;
+    string result;
+    std::getline(cin, result);
+    return result;
 }
 
 void OpenUserFile(ifstream& input) {
-	while (true) {
-		cout << "Enter filename: ";
-		string filename = GetLine();
+    while (true) {
+        cout << "Enter filename: ";
+        string filename = GetLine();
 
-		input.open(filename.c_str());
-		if (input.is_open()) break;
+        input.open(filename.c_str());
+        if (input.is_open()) break;
 
-		cout << "Sorry, cannot find file " << filename << endl;
-		input.clear();
-	}
+        cout << "Sorry, cannot find file " << filename << endl;
+        input.clear();
+    }
 }
 
 pointT MakePoint(int row, int col) {
-	pointT result;
-	result.row = row;
-	result.col = col;
-	return result;
+    pointT result;
+    result.row = row;
+    result.col = col;
+    return result;
 }
 
 void LoadWorld(gameT& game, ifstream& input) {
-	input >> game.numRows >> game.numCols;
-	game.world.resize(game.numRows);
+    input >> game.numRows >> game.numCols;
+    game.world.resize(game.numRows);
 
-	input >> game.dx >> game.dy;
+    input >> game.dx >> game.dy;
 
-	string dummy;
-	std::getline(input, dummy);
+    string dummy;
+    std::getline(input, dummy);
 
-	for (int row = 0; row < game.numRows; ++row) {
-		std::getline(input, game.world[row]);
-		int col = game.world[row].find(kSnakeTile);
-		if (col != string::npos) {
-			game.snake.push_back(MakePoint(row, col));
-		}
-	}
-	for (int row = 0; row < game.numRows; ++row)
-		cout << game.world[row] << endl;
-	game.numEaten = 0;
+    for (int row = 0; row < game.numRows; ++row) {
+        std::getline(input, game.world[row]);
+        int col = game.world[row].find(kSnakeTile);
+        if (col != string::npos) {
+            game.snake.push_back(MakePoint(row, col));
+        }
+    }
+    for (int row = 0; row < game.numRows; ++row)
+        cout << game.world[row] << endl;
+    game.numEaten = 0;
 }
 
 void InitializeGame(gameT& game) {
-	srand(static_cast<unsigned int>(time(NULL)));
+    srand(static_cast<unsigned int>(time(NULL)));
 
-	ifstream input;
-	OpenUserFile(input);
-	LoadWorld(game, input);
+    ifstream input;
+    OpenUserFile(input);
+    LoadWorld(game, input);
 }
 
 const double kWaitTime = 0.1;
 
 void Pause() {
-	clock_t startTIme = clock();
-	while (static_cast<double>(clock() - startTIme) / CLOCKS_PER_SEC < kWaitTime)
-		;
+    clock_t startTIme = clock();
+    while (static_cast<double>(clock() - startTIme) / CLOCKS_PER_SEC <
+           kWaitTime)
+        ;
 }
 
 const string kClearCommand = "clear";
 
 void PrintWorld(gameT& game) {
-	system(kClearCommand.c_str());
-	for (int row = 0; row < game.numRows; ++row)
-		cout << game.world[row] << endl;
-	cout << "Food eaten: " << game.numEaten << endl;
+    system(kClearCommand.c_str());
+    for (int row = 0; row < game.numRows; ++row)
+        cout << game.world[row] << endl;
+    cout << "Food eaten: " << game.numEaten << endl;
 }
 
 void DisplayResult(gameT& game) {
-	PrintWorld(game);
-	if (game.numEaten == kMaxFood) {
-		cout << "The snake ate enough food and wins!" << endl;
-	} else {
-		cout << "Oh no! The snake crashed!" << endl;
-	}
+    PrintWorld(game);
+    if (game.numEaten == kMaxFood) {
+        cout << "The snake ate enough food and wins!" << endl;
+    } else {
+        cout << "Oh no! The snake crashed!" << endl;
+    }
 }
 
 pointT GetNextPosition(gameT& game, int dx, int dy) {
-	pointT result = game.snake.front();
+    pointT result = game.snake.front();
 
-	result.row += dy;
-	result.col += dx;
-	return result;
+    result.row += dy;
+    result.col += dx;
+    return result;
 }
 
 bool InWorld(pointT& pt, gameT& game) {
-	return pt.col >= 0 && pt.row >= 0 &&
-		pt.col < game.numCols && pt.row < game.numRows;
+    return pt.col >= 0 && pt.row >= 0 && pt.col < game.numCols &&
+           pt.row < game.numRows;
 }
 
 bool Crashed(pointT headPos, gameT& game) {
-	return !InWorld(headPos, game) ||
-		game.world[headPos.row][headPos.col] == kSnakeTile ||
-		game.world[headPos.row][headPos.col] == kWallTile;
+    return !InWorld(headPos, game) ||
+           game.world[headPos.row][headPos.col] == kSnakeTile ||
+           game.world[headPos.row][headPos.col] == kWallTile;
 }
 
 bool RandomChance(double probability) {
-	return (rand() / (RAND_MAX + 1.0)) < probability;
+    return (rand() / (RAND_MAX + 1.0)) < probability;
 }
 
 const double kTurnRate = 0.2;
 void PerformAI(gameT& game) {
-	pointT nextHead = GetNextPosition(game, game.dx, game.dy);
+    pointT nextHead = GetNextPosition(game, game.dx, game.dy);
 
-	if (Crashed(nextHead, game) || RandomChance(kTurnRate)) {
-		int leftDx = -game.dy;
-		int leftDy = game.dx;
-		int rightDx = game.dy;
-		int rightDy = -game.dx;
+    if (Crashed(nextHead, game) || RandomChance(kTurnRate)) {
+        int leftDx = -game.dy;
+        int leftDy = game.dx;
+        int rightDx = game.dy;
+        int rightDy = -game.dx;
 
-		bool canLeft = !Crashed(GetNextPosition(game, leftDx, leftDy), game);
-		bool canRight = !Crashed(GetNextPosition(game, rightDx, rightDy), game);
+        bool canLeft = !Crashed(GetNextPosition(game, leftDx, leftDy), game);
+        bool canRight = !Crashed(GetNextPosition(game, rightDx, rightDy), game);
 
-		bool willTurnLeft = false;
-		if (!canLeft && !canRight)
-			return;
-		else if (canLeft && !canRight)
-			willTurnLeft = true;
-		else if (!canLeft && canRight)
-			willTurnLeft = false;
-		else
-			willTurnLeft = RandomChance(0.5);
+        bool willTurnLeft = false;
+        if (!canLeft && !canRight)
+            return;
+        else if (canLeft && !canRight)
+            willTurnLeft = true;
+        else if (!canLeft && canRight)
+            willTurnLeft = false;
+        else
+            willTurnLeft = RandomChance(0.5);
 
-		game.dx = willTurnLeft ? leftDx : rightDx;
-		game.dy = willTurnLeft ? leftDy : rightDy;
-	}
+        game.dx = willTurnLeft ? leftDx : rightDx;
+        game.dy = willTurnLeft ? leftDy : rightDy;
+    }
 }
 
 void PlaceFood(gameT& game) {
-	while (true) {
-		int row = rand() % game.numRows;
-		int col = rand() % game.numCols;
+    while (true) {
+        int row = rand() % game.numRows;
+        int col = rand() % game.numCols;
 
-		if (game.world[row][col] == kEmptyTile) {
-			game.world[row][col] = kFoodTile;
-			return;
-		}
-	}
+        if (game.world[row][col] == kEmptyTile) {
+            game.world[row][col] = kFoodTile;
+            return;
+        }
+    }
 }
 
 bool MoveSnake(gameT& game) {
-	pointT nextHead = GetNextPosition(game, game.dx, game.dy);
-	if (Crashed(nextHead, game))
-		return false;
-	bool isFood = (game.world[nextHead.row][nextHead.col] == kFoodTile);
+    pointT nextHead = GetNextPosition(game, game.dx, game.dy);
+    if (Crashed(nextHead, game)) return false;
+    bool isFood = (game.world[nextHead.row][nextHead.col] == kFoodTile);
 
-	game.world[nextHead.row][nextHead.col] = kSnakeTile;
-	game.snake.push_front(nextHead);
+    game.world[nextHead.row][nextHead.col] = kSnakeTile;
+    game.snake.push_front(nextHead);
 
-	if (!isFood) {
-		game.world[game.snake.back().row][game.snake.back().col] = kEmptyTile;
-		game.snake.pop_back();
-	} else {
-		++game.numEaten;
-		PlaceFood(game);
-	}
-	return true;
+    if (!isFood) {
+        game.world[game.snake.back().row][game.snake.back().col] = kEmptyTile;
+        game.snake.pop_back();
+    } else {
+        ++game.numEaten;
+        PlaceFood(game);
+    }
+    return true;
 }
 
 void RunSimulation(gameT& game) {
-	while (game.numEaten < kMaxFood) {
-		PrintWorld(game);
-		PerformAI(game);
+    while (game.numEaten < kMaxFood) {
+        PrintWorld(game);
+        PerformAI(game);
 
-		if (!MoveSnake(game))
-			break;
-		Pause();
-	}
-	DisplayResult(game);
+        if (!MoveSnake(game)) break;
+        Pause();
+    }
+    DisplayResult(game);
 }
 
 int main() {
-	gameT game;
-	InitializeGame(game);
-	RunSimulation(game);
-	return 0;
+    gameT game;
+    InitializeGame(game);
+    RunSimulation(game);
+    return 0;
 }
